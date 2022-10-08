aa90f99e8d74bb2108576af2b964b49b6e88e57b
Tom <tom.lucero@me.com>
2022-10-07 18:12:56 -0600

    adds unit testing

---
+++ tests/unit/gitrepo.spec.ts
@@ -0,0 +1,19 @@
+const {describe, it, expect} = require('@jest/globals')
+
+describe('github api tests', () => {
+    it('should fetch valid response', () => {
+    });
+
+    it('should fail with non github url', () => {
+        
+    });
+    it('should fail with malformed github url', () => {
+
+    });
+});
+
+describe('invalid url tests', () => {
+    it('check root url', () => {
+
+    })
+})
