459b367b8f269474e76d7a80e0147c345b6fb6e7
Tom <tom.lucero@me.com>
2022-10-09 09:05:24 -0600

    adds tests for other files

--- tests/unit/gitrepo.spec.ts
+++ tests/unit/gitrepo.spec.ts
@@ -1,19 +1,59 @@
-const {describe, it, expect} = require('@jest/globals')
+const jest = require('@jest/globals')
 
-describe('github api tests', () => {
-    it('should fetch valid response', () => {
+jest.describe('github api tests', () => {
+    jest.describe('POST functions', () =>{
+        jest.it('gitRepoPostRequest with valid URL in body', () => {
+
+        });
+
+        jest.it('gitRepoPostRequest with invalid URL in body', () => {
+
+        });
+
+        jest.it('gitRepoPostRequest with no body in URL', () => {
+
+        });
     });
 
-    it('should fail with non github url', () => {
-        
+    jest.describe('GET functions', () => {
+        jest.it('gitRepoGetRequest with no parameters in address', () => {
+
+        });
+
+        jest.it('gitRepoGetRequest with parameters in address', () => {
+
+        });
+
+        jest.it('getGitHubResponse with url', () => {
+
+        });
+
+        jest.it('getGitHubResponse without url', () => {
+
+        });
+
+        jest.it('getNumberOfCommits with empty array', () => {
+
+        });
+
+        jest.it('getNumberOfCommits with data object in array', () => {
+
+        });
     });
-    it('should fail with malformed github url', () => {
 
-    });
-});
+    jest.describe('data processing functions', () => {
+        jest.it('extract path from valid URL', () => {
+        });
 
-describe('invalid url tests', () => {
-    it('check root url', () => {
+        jest.it('extract path that contains ending / or not', () => {
 
+        });
+        jest.it('extract path with space in address', () => {
+
+        });
+
+        jest.it('should handle \'//\' in url address', () => {
+
+        });
     })
-})
+});
