7df5b8386fa8b33519b8c916c90b6526a54ae4c9
Tom <tom.lucero@me.com>
2022-10-11 22:36:32 -0600

    updates misc_routes tests

--- tests/unit/misc_routes.spec.ts
+++ tests/unit/misc_routes.spec.ts
@@ -1,9 +1,40 @@
+import {
+    describe,
+    it,
+    expect,
+    jest
+} from '@jest/globals'
+import {
+    MockRequestOptions,
+    Request,
+    Response
+} from 'mock-http';
+import { invalidRequest, rootGetRequest } from '../../routes/misc_routes';
+
 describe('misc_routes tests', () => {
     it('should get root get request', () => {
+        const req = new Request({
+            url: '/',
+            method: 'GET'
+        } as MockRequestOptions)
+        const res = new Response({
 
+        } as MockRequestOptions);
+        expect(res.statusCode).toBeFalsy()
+        rootGetRequest(req, res);
+        expect(res.statusCode).toBe(200);
     });
 
     it('should send invalid page response', () => {
+        const req = new Request({
+            url: '/',
+            method: 'GET'
+        } as MockRequestOptions)
+        const res = new Response({
 
+        } as MockRequestOptions);
+        expect(res.statusCode).toBeFalsy()
+        invalidRequest(req, res);
+        expect(res.statusCode).toBe(404);
     });
 })
