ea14458da3156f41d8bb4196ebd654361407f542
Tom <tom.lucero@me.com>
2022-10-11 22:46:08 -0600

    updates tests for route_utils

--- tests/unit/route_utils.spec.ts
+++ tests/unit/route_utils.spec.ts
@@ -1,9 +1,32 @@
+import {
+    describe,
+    it,
+    expect,
+    jest
+} from '@jest/globals'
+import {
+    MockRequestOptions,
+    Request,
+    Response
+} from 'mock-http';
+import { updateHeadersAndSendResponseData } from '../../routes/route_utils';
+
 describe('route utils tests', () => {
     it('should update headers and send 200 response with data', () => {
-
+        const res = new Response({} as MockRequestOptions);
+        expect(res.statusCode).toBeFalsy()
+        updateHeadersAndSendResponseData(200, {}, res)
+        expect(res.statusCode).toBe(200);
+        console.log(res.getHeaders()['content-type'])
+        expect(res.getHeaders()['content-type']).toBe('application/json');
     });
 
     it('should update headers and send 400 response with error data', () => {
-
+        const res = new Response({} as MockRequestOptions);
+        expect(res.statusCode).toBeFalsy()
+        updateHeadersAndSendResponseData(400, {}, res)
+        expect(res.statusCode).toBe(400);
+        console.log(res.getHeaders()['content-type'])
+        expect(res.getHeaders()['content-type']).toBe('application/json');
     });
 })
